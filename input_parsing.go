3f5eeb7640ab24e91339082e99714b64400f5cb8
Fab-Den <denoyelle.fabien@gmail.com>
2024-01-08 17:46:03 +0100

    Parsing

--- input_parsing.go
+++ input_parsing.go
@@ -13,6 +13,160 @@ type Variable struct {
 	ran  [2]float64
 }
 
+type Ineq struct {
+	// left < right
+	str   string
+	left  Operation
+	right Operation
+}
+type Operation struct {
+	expression string
+	operator   rune
+	elements   []Operation
+}
+
+func (I *Ineq) evaluate(listVar []Variable, varValues []float64) bool {
+	if I.left.getValue(listVar, varValues) < I.right.getValue(listVar, varValues) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (I *Ineq) innit() {
+	I.left.expression = ""
+	I.right.expression = ""
+
+	i := 0
+	for I.str[i] != '<' && I.str[i] != '>' {
+		I.left.expression += string(I.str[i])
+		i += 1
+	}
+
+	separator := I.str[i]
+
+	i += 1
+
+	for i < len(I.str) {
+		I.right.expression += string(I.str[i])
+		i += 1
+	}
+
+	if separator == '>' {
+		I.left, I.right = I.right, I.left
+	}
+
+	println("Banane\n")
+
+	I.left.innit()
+	I.right.innit()
+}
+
+func (O *Operation) innit() {
+	substring := O.expression
+
+	ind := findIndexOfChar(substring, '(')
+	ind2 := findLastIndexOfChar(substring, ')')
+
+	println(ind, ind2)
+	if ind == 0 && ind2 == len(substring)-1 {
+		O.elements = append(O.elements, Operation{expression: substring[1 : len(substring)-1]})
+		O.operator = '('
+	} else {
+		op := 'n'
+		indexOp := 0
+		listOp := []rune{'-', '+', '*', '/'}
+
+		for _, currentOp := range listOp {
+			sub := O.expression
+
+			currentOpIndex := findIndexOfChar(O.expression, currentOp)
+
+			for currentOpIndex != -1 {
+
+				if currentOpIndex != -1 && (currentOpIndex < ind || currentOpIndex > ind2) {
+					op = currentOp
+					indexOp = currentOpIndex
+					break
+				} else {
+					sub = O.expression[currentOpIndex+1:]
+					println(sub, "\n")
+					find := findIndexOfChar(sub, currentOp)
+					if find == -1 {
+						currentOpIndex = -1
+					} else {
+						currentOpIndex = currentOpIndex + 1 + find
+					}
+
+				}
+
+			}
+			if op != 'n' {
+				break
+			}
+		}
+
+		O.operator = op
+
+		if op != 'n' {
+
+			newO := Operation{expression: O.expression[0:indexOp]}
+			if len(O.expression) != 0 {
+				O.elements = append(O.elements, newO)
+			}
+
+			newO = Operation{expression: O.expression[indexOp+1:]}
+			if len(O.expression) != 0 {
+				O.elements = append(O.elements, newO)
+			}
+
+		}
+	}
+	if O.elements != nil && len(O.elements) > 0 {
+		for i := range O.elements {
+			O.elements[i].innit()
+		}
+	}
+}
+
+func (O *Operation) getValue(listVar []Variable, varValues []float64) float64 {
+
+	println(O.expression)
+
+	if O.operator == 'n' {
+		floatValue, err := strconv.ParseFloat(O.expression, 64)
+		if err == nil {
+			return floatValue
+		}
+
+		varIndex := findVariableIndex(listVar, O.expression)
+
+		if varIndex != -1 {
+			return varValues[varIndex]
+		}
+
+	} else {
+
+		if O.operator == '*' {
+			return O.elements[0].getValue(listVar, varValues) * O.elements[1].getValue(listVar, varValues)
+
+		} else if O.operator == '/' {
+			return O.elements[0].getValue(listVar, varValues) / O.elements[1].getValue(listVar, varValues)
+
+		} else if O.operator == '+' {
+			return O.elements[0].getValue(listVar, varValues) + O.elements[1].getValue(listVar, varValues)
+
+		} else if O.operator == '-' {
+			return O.elements[0].getValue(listVar, varValues) - O.elements[1].getValue(listVar, varValues)
+
+		} else if O.operator == '(' {
+			return O.elements[0].getValue(listVar, varValues)
+		}
+	}
+	println("TA MERE", O.operator, O.expression)
+	return 0
+}
+
 func (V *Variable) setVariableRange(file *os.File) error {
 
 	scanner := bufio.NewScanner(file)
@@ -83,4 +237,25 @@ func findVariableIndex(listVar []Variable, name string) int {
 		}
 	}
 	return -1
+}
+
+func findIndexOfChar(str string, charR rune) int {
+	println(str, "\n")
+	for i, char := range str {
+		if char == charR {
+			return i
+		}
+	}
+	return -1
+}
+
+func findLastIndexOfChar(str string, charR rune) int {
+	lastIndex := -1
+
+	for i, char := range str {
+		if char == charR {
+			lastIndex = i
+		}
+	}
+	return lastIndex
 }
