a03146c714a48d299d5c8aea323291ab745d8bc2
Lpoulhe <154341987+Lpoulhe@users.noreply.github.com>
2024-01-12 14:18:52 +0100

    client

--- tcp_client.go
+++ tcp_client.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	// Connect to the server
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Send data to the server
+	data := []byte("Hello, Server!")
+	_, err = conn.Write(data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+}
