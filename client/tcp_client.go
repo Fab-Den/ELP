b4491e255450626ae151408efb4845218f97e6df
Lpoulhe <154341987+Lpoulhe@users.noreply.github.com>
2024-01-12 17:16:19 +0100

    client loop

--- client/tcp_client.go
+++ client/tcp_client.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
-	// "time"
+	"strings"
 )
 
 func main() {
@@ -16,58 +16,75 @@ func main() {
 		return
 	}
 	defer conn.Close()
-
 	var input string
 	var choice string
 	var str string
-	//Ask the user if he has an input file ready
-	fmt.Print("Do you have an input file? [y/n] ")
-	scanner_choice := bufio.NewScanner(os.Stdin)
-	if scanner_choice.Scan() {
-		choice = scanner_choice.Text()
-	}
-	//if yes, ask for the path, retrieve it open the file and reads it
-	if choice == "y" || choice == "Y" {
-		fmt.Println("Enter file path: ")
-		scanner_file := bufio.NewScanner(os.Stdin)
-		if scanner_file.Scan() {
-			file_path := scanner_file.Text()
-			fmt.Println(file_path)
-			input = file_reading(file_path)
-		}
-		// if not, ask the user to directly submit the data
-	} else if choice == "N" || choice == "n" {
-		fmt.Println("Enter your data: ")
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			line := scanner.Text()
 
-			if line == "" {
-				break
+	for {
+		//Ask the user if he has an input file ready
+		fmt.Print("Do you have an input file? [y/n] ")
+		scanner_choice := bufio.NewScanner(os.Stdin)
+		if scanner_choice.Scan() {
+			choice = scanner_choice.Text()
+		}
+		//if yes, ask for the path, retrieve it open the file and reads it
+		if choice == "y" || choice == "Y" {
+			fmt.Println("Enter file path: ")
+			scanner_file := bufio.NewScanner(os.Stdin)
+			if scanner_file.Scan() {
+				file_path := scanner_file.Text()
+				fmt.Println(file_path)
+				input = file_reading(file_path)
 			}
-			input += line + "\n"
-		}
+			// if not, ask the user to directly submit the data
+		} else if choice == "N" || choice == "n" {
+			fmt.Println("Enter your data: ")
+			scanner := bufio.NewScanner(os.Stdin)
+			for scanner.Scan() {
+				line := scanner.Text()
 
-		if err := scanner.Err(); err != nil {
-			fmt.Println("Error reading input:", err)
+				if line == "" {
+					break
+				}
+				input += line + "\n"
+			}
+
+			if err := scanner.Err(); err != nil {
+				fmt.Println("Error reading input:", err)
+			}
+		} else if choice == "quit" {
+			fmt.Println("Closing client")
 			return
 		}
-	}
-	// if the input, from the file or the console, is empty then return an error
-	if input == "" || input == "error" {
-		fmt.Println("Error in your data, unable to send")
-		//else add an identification sequence at the end of the string
-	} else {
-		str = input + "end"
-		fmt.Println(str)
-	}
+		// if the input, from the file or the console, is empty then return an error
+		if input == "" || input == "error" {
+			fmt.Println("Error in your data, unable to send")
+			//else add an identification sequence at the end of the string
+		} else {
+			str = input + "end"
+			fmt.Println(str)
+		}
 
-	// Send data to the server
-	data := []byte(str)
-	_, err = conn.Write(data)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		// Send data to the server
+		data := []byte(str)
+		_, err = conn.Write(data)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+
+		var rec []byte
+		buffer := make([]byte, 1024)
+		for !strings.Contains(string(rec), "end") {
+			// Read data from the server
+			n, err := conn.Read(buffer)
+			if err != nil {
+				fmt.Println("Error reading:", err)
+				return
+			}
+
+			rec = append(rec, buffer[:n]...)
+		}
+		fmt.Println(rec)
 	}
 }
 
