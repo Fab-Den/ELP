bb9082c9b8e0863f06cea9f11b1ab8311b0547e6
Fab-Den <denoyelle.fabien@gmail.com>
2024-01-13 11:48:46 +0100

    Add docstrings

--- server/input_parsing.go
+++ server/input_parsing.go
@@ -6,27 +6,38 @@ import (
 	"strings"
 )
 
+// Variable is used to represent a variable from the formulas given in input. The structure is used to associate
+// the variable to the range.
 type Variable struct {
 	name string
 	ran  [2]float64
 }
 
+// Inequalities is a simple structure to make simpler the argument passing and the evaluation of the inequalities
 type Inequalities struct {
 	inequalities []Inequality
 }
 
+// Inequality represents one formula given in input. It stores the two members of the inequality. The left member is
+// lower than the right member.
 type Inequality struct {
-	// left < right
 	str   string
 	left  Operation
 	right Operation
 }
+
+// Operation stores an operation from the formula. The expression attribute is the string associated to the operation.
+// Operator is a character to know which mathematical operation is associated. The 'n' value in this attribute
+// represents the fact that there is not an operation to carry out, and the object represents then a single value.
+// As an operation is carried out between two members, the elements attribute can store two other Operation structure.
 type Operation struct {
 	expression string
 	operator   rune
 	elements   []Operation
 }
 
+// evaluate is a method for the Inequality structure, which is called when we want to know if the inequality is
+// respected according to the values of variables given in parameters.
 func (I *Inequality) evaluate(listVar []Variable, varValues []float64) bool {
 
 	if I.left.getValue(listVar, varValues) < I.right.getValue(listVar, varValues) {
@@ -36,6 +47,8 @@ func (I *Inequality) evaluate(listVar []Variable, varValues []float64) bool {
 	}
 }
 
+// evaluate is a method for Inequalities that calls evaluate for each Inequality. Returns if all the inequalities
+// are respected.
 func (I *Inequalities) evaluate(listVar []Variable, varValues []float64) bool {
 	for _, inequality := range I.inequalities {
 		if !inequality.evaluate(listVar, varValues) {
@@ -45,7 +58,9 @@ func (I *Inequalities) evaluate(listVar []Variable, varValues []float64) bool {
 	return true
 }
 
-func (I *Inequality) innit() {
+// init is a method for Inequality, which parse the inequality string into the Operation instances stored into the left
+// and right attributes. At the end of the parsing, call the init for each Operation instance.
+func (I *Inequality) init() {
 	I.left.expression = ""
 	I.right.expression = ""
 
@@ -68,16 +83,20 @@ func (I *Inequality) innit() {
 		I.left, I.right = I.right, I.left
 	}
 
-	I.left.innit()
-	I.right.innit()
+	I.left.init()
+	I.right.init()
 }
 
-func (O *Operation) innit() {
+// init is a method for Operation which parse the expression string attribute of the object into the element attribute.
+func (O *Operation) init() {
 	substring := O.expression
 
+	// we get the index of the first opening parenthesis and the index of the last closing parenthesis
 	ind := findIndexOfChar(substring, '(')
 	ind2 := findLastIndexOfChar(substring, ')')
 
+	// if parenthesis encompass all the formula, we create a single Operation with the content of parenthesis as
+	// expression attribute
 	if ind == 0 && ind2 == len(substring)-1 {
 		O.elements = append(O.elements, Operation{expression: substring[1 : len(substring)-1]})
 		O.operator = '('
@@ -86,6 +105,7 @@ func (O *Operation) innit() {
 		indexOp := 0
 		listOp := []rune{'-', '+', '*', '/'}
 
+		// in the order of listOp, we get the first operation outside parenthesis
 		for _, currentOp := range listOp {
 			sub := O.expression
 
@@ -116,6 +136,8 @@ func (O *Operation) innit() {
 
 		O.operator = op
 
+		// if there is an operation to carry out, we add the left and right member as Operation instance in the
+		// elements attribute
 		if op != 'n' {
 
 			newO := Operation{expression: O.expression[0:indexOp]}
@@ -130,13 +152,17 @@ func (O *Operation) innit() {
 
 		}
 	}
+
+	// if there are Operation instances in element attributes, we initialize all of them
 	if O.elements != nil && len(O.elements) > 0 {
 		for i := range O.elements {
-			O.elements[i].innit()
+			O.elements[i].init()
 		}
 	}
 }
 
+// getValue is a method for Operation which gets the value of an Operation object according to values for variables,
+// that are given in parameters. Returns then the value.
 func (O *Operation) getValue(listVar []Variable, varValues []float64) float64 {
 
 	if O.operator == 'n' {
@@ -172,6 +198,8 @@ func (O *Operation) getValue(listVar []Variable, varValues []float64) float64 {
 	return 0
 }
 
+// setVariableRange is a method vor Variable which sets the ran attribute of Variable object according to the input
+// string.
 func (V *Variable) setVariableRange(str string) error {
 
 	lines := strings.Split(str, "\n")
@@ -212,6 +240,7 @@ func (V *Variable) setVariableRange(str string) error {
 	return nil
 }
 
+// initializeVariables returns a list of variables according to the input string.
 func initializeVariables(str string) ([]Variable, error) {
 
 	var listOfVariables []Variable
@@ -227,6 +256,8 @@ func initializeVariables(str string) ([]Variable, error) {
 	return listOfVariables, nil
 }
 
+// initializeInequalities returns an Inequalities object (list of Inequality) according to the input string. Also, it
+// calls the init method for each Inequality instance.
 func initializeInequalities(str string) (Inequalities, error) {
 	lines := strings.Split(str, "\n")
 
@@ -240,12 +271,13 @@ func initializeInequalities(str string) (Inequalities, error) {
 	}
 
 	for i := range Is.inequalities {
-		Is.inequalities[i].innit()
+		Is.inequalities[i].init()
 	}
 
 	return Is, nil
 }
 
+// getNumberOfPoints is a function that gets the number of points requested in the input file and returns the value.
 func getNumberOfPoints(str string) int {
 	lines := strings.Split(str, "\n")
 	for _, line := range lines {
@@ -262,9 +294,9 @@ func getNumberOfPoints(str string) int {
 	return 0
 }
 
+// getSpaceVolume is a function that returns the total volume of the space, according to the range of each variable.
 func getSpaceVolume(listVars []Variable) float64 {
-	var total float64
-	total = 1
+	total := float64(1)
 
 	for _, variable := range listVars {
 		total *= variable.ran[1] - variable.ran[0]
@@ -274,6 +306,7 @@ func getSpaceVolume(listVars []Variable) float64 {
 
 }
 
+// initVariableRange is a function that calls the setVariableRange for each Variable in listVar.
 func initVariableRange(listVar []Variable, str string) error {
 
 	for i := range listVar {
@@ -286,6 +319,8 @@ func initVariableRange(listVar []Variable, str string) error {
 	return nil
 }
 
+// findVariableIndex is a function that returns the index of a variable in a list of variables according to its name.
+// returns -1 if the variable is not found.
 func findVariableIndex(listVar []Variable, name string) int {
 	for i, V := range listVar {
 		if V.name == name {
@@ -295,6 +330,8 @@ func findVariableIndex(listVar []Variable, name string) int {
 	return -1
 }
 
+// findIndexOfChar is a function that returns the first index of the rune charR in the string str, or -1 if str doesn't
+// contain the charR.
 func findIndexOfChar(str string, charR rune) int {
 	for i, char := range str {
 		if char == charR {
@@ -304,6 +341,8 @@ func findIndexOfChar(str string, charR rune) int {
 	return -1
 }
 
+// findLastIndexOfChar is a function that returns the last index of the rune charR in the string str, or -1 if str
+// doesn't contain the charR.
 func findLastIndexOfChar(str string, charR rune) int {
 	lastIndex := -1
 
@@ -315,6 +354,7 @@ func findLastIndexOfChar(str string, charR rune) int {
 	return lastIndex
 }
 
+// countCharInString is a function that counts the number of rune char that the string str contains.
 func countCharInString(str string, char rune) int {
 	out := 0
 	for _, v := range str {
