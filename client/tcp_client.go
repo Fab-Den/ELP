4c57a16f2b6584d2245f4c0e25c85229b94546b1
Fab-Den <denoyelle.fabien@gmail.com>
2024-01-15 15:08:31 +0100

    Add choice of host address

--- client/tcp_client.go
+++ client/tcp_client.go
@@ -8,94 +8,119 @@ import (
 	"time"
 )
 
+const defaultPort = "8080"
+
 func main() {
-	// Connect to the server
-	conn, err := net.Dial("tcp", "localhost:8080")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer conn.Close()
-	var input string
-	var choice string
-	var str string
+	var conn net.Conn
+	var err error
 
 	for {
-		//Ask the user if he has an input file ready
-		fmt.Print("Do you have an input file? [y/n] ")
-		scanner_choice := bufio.NewScanner(os.Stdin)
-		if scanner_choice.Scan() {
-			choice = scanner_choice.Text()
-		}
-		//if yes, ask for the path, retrieve it open the file and reads it
-		if choice == "y" || choice == "Y" {
-			fmt.Println("Enter file path: ")
-			scanner_file := bufio.NewScanner(os.Stdin)
-			if scanner_file.Scan() {
-				file_path := scanner_file.Text()
-				fmt.Println(file_path)
-				input = file_reading(file_path)
-			}
-			// if not, ask the user to directly submit the data
-		} else if choice == "N" || choice == "n" {
-			fmt.Println("Enter your data: ")
-			scanner := bufio.NewScanner(os.Stdin)
-			for scanner.Scan() {
-				line := scanner.Text()
+		fmt.Println("Server address : ")
+		addressChoice := bufio.NewScanner(os.Stdin)
 
-				if line == "" {
-					break
-				}
-				input += line + "\n"
-			}
+		addressChoice.Scan()
 
-			if err := scanner.Err(); err != nil {
-				fmt.Println("Error reading input:", err)
-			}
-		} else if choice == "quit" {
-			fmt.Println("Closing client")
-			return
-		}
-		// if the input, from the file or the console, is empty then return an error
-		if input == "" || input == "error" {
-			fmt.Println("Error in your data, unable to send")
-			//else add an identification sequence at the end of the string
-		} else {
-			str = input + "end"
-			fmt.Println(str)
-		}
-
-		// Send data to the server
-		data := []byte(str)
-		_, err = conn.Write(data)
+		// Connect to the server
+		conn, err = net.Dial("tcp", addressChoice.Text()+":"+defaultPort)
 		if err != nil {
 			fmt.Println("Error:", err)
+		} else {
+			break
 		}
+	}
 
-		timeStart := time.Now()
+	if conn != nil {
 
-		var rec []byte
-		buffer := make([]byte, 1024)
-		// Read data from the server
-		n, err := conn.Read(buffer)
-		if err != nil {
-			fmt.Println("Error reading:", err)
-			return
+		defer func(conn net.Conn) {
+			err := conn.Close()
+			if err != nil {
+				fmt.Println("Error when closing connection : ", err)
+			}
+		}(conn)
+
+		var input string
+		var choice string
+		var str string
+
+		for {
+			//Ask the user if he has an input file ready
+			fmt.Print("Do you have an input file? [y/n] ")
+			scannerChoice := bufio.NewScanner(os.Stdin)
+			if scannerChoice.Scan() {
+				choice = scannerChoice.Text()
+			}
+
+			//if yes, ask for the path, retrieve it open the file and reads it
+			if choice == "y" || choice == "Y" {
+				fmt.Println("Enter file path: ")
+				scannerFile := bufio.NewScanner(os.Stdin)
+				if scannerFile.Scan() {
+					filePath := scannerFile.Text()
+					fmt.Println(filePath)
+					input = fileReading(filePath)
+				}
+
+				// if not, ask the user to directly submit the data
+			} else if choice == "N" || choice == "n" {
+				fmt.Println("Enter your data: ")
+				scanner := bufio.NewScanner(os.Stdin)
+				for scanner.Scan() {
+					line := scanner.Text()
+
+					if line == "" {
+						break
+					}
+					input += line + "\n"
+				}
+
+				if err := scanner.Err(); err != nil {
+					fmt.Println("Error reading input:", err)
+				}
+			} else if choice == "quit" {
+				fmt.Println("Closing client")
+				return
+			}
+			// if the input, from the file or the console, is empty then return an error
+			if input == "" || input == "error" {
+				fmt.Println("Error in your data, unable to send")
+				//else add an identification sequence at the end of the string
+			} else {
+				str = input + "end"
+				fmt.Println(str)
+			}
+
+			// Send data to the server
+			data := []byte(str)
+			_, err = conn.Write(data)
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+
+			timeStart := time.Now()
+
+			var rec []byte
+			buffer := make([]byte, 1024)
+			// Read data from the server
+			n, err := conn.Read(buffer)
+			if err != nil {
+				fmt.Println("Error reading:", err)
+				return
+			}
+			rec = append(rec, buffer[:n]...)
+
+			fmt.Println(string(rec))
+
+			println("Execution time for :", time.Since(timeStart)/time.Millisecond, "ms")
+
 		}
-		rec = append(rec, buffer[:n]...)
-
-		fmt.Println(string(rec))
-
-		println("Execution time for :", time.Since(timeStart)/time.Millisecond, "ms")
-
 	}
 }
 
-func file_reading(path string) string {
+func fileReading(path string) string {
 	var str string
 	file, err := os.Open(path)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		fmt.Println("Error opening file : ", err)
 		return "error"
 	}
 
@@ -108,7 +133,7 @@ func file_reading(path string) string {
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
-
+			fmt.Println("Error closing file : ", err)
 		}
 	}(file)
 
