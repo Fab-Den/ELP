65383ea05a3d5a04d21715de94a9d54958c40622
Lpoulhe <154341987+Lpoulhe@users.noreply.github.com>
2024-01-22 20:51:47 +0100

    All functionnalities working including auto start and checkbox for answer

--- ELM/src/http_request.elm
+++ ELM/src/http_request.elm
@@ -2,7 +2,7 @@ module Http_request exposing (..)
 
 import Browser
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 import Json.Decode exposing (Decoder, map2, field, string, at)
 import Html.Attributes exposing (..)
 import Http
@@ -31,6 +31,8 @@ type alias Model =
     , word_list : List String
     , current_word : String
     , random_index : Int
+    , input : String
+    , checked : Bool
     }
 
 type alias Definition =
@@ -42,7 +44,10 @@ init_model = {state = Success
     , definition = []
     , word_list = ["hello", "blue", "submarine", "forest","computer"]
     , current_word = ""
-    , random_index = 0}
+    , random_index = 0
+    , input = ""
+    , checked = False}
+
 init : () -> (Model, Cmd Msg)
 init _ =
     (init_model
@@ -63,6 +68,8 @@ type Msg
     | Roll
     | GetElementFromList Int
     | GotText (Result Http.Error String)
+    | Change String
+    | ToggleCheck
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -96,10 +103,17 @@ update msg model =
         GotText result ->
             case result of
                 Ok fullText ->
-                    ( {model|word_list = String.split " " fullText, state = Success} , Cmd.none)
+                    ( {model|word_list = String.split " " fullText, state = Success} 
+                    , Random.generate GetElementFromList (roll (List.length model.word_list)))
 
                 Err _ ->
                     (model, Cmd.none)
+        Change newInput -> 
+            ({model | input = newInput}, Cmd.none)
+        ToggleCheck ->
+            ({model | checked = not(model.checked)}, Cmd.none)
+
+
 
 
 -- SUBSCRIPTIONS
@@ -132,23 +146,38 @@ viewQuote model =
 
         Success  -> 
             div []
-            [ h3 [] [ text (model.current_word)]
+            [ displayWord model
             , div [] (List.map displayDefinition model.definition)
-            , button [ onClick Roll ] [ text "Roll" ]
+            , checkGuess model
+            , input [ placeholder "Guess the word!", value model.input, onInput Change] []
+            , button [ onClick Roll ] [ text "Change word" ]
+            , input [type_ "checkbox", checked model.checked, onClick ToggleCheck] [], text "Show answer"
             ]
 
-displayTest : String -> Html Msg
-displayTest word = 
-    li [] [text word]
 
-displayDefinition : Definition -> Html msg
+checkGuess : Model -> Html Msg
+checkGuess model =
+    if model.input == model.current_word then
+        div [ style "color" "green"] [ text ("You found the word \""++model.current_word++"\" !")]
+    else 
+        div [ style "color" "red"] [ text "Not quite there yet"]
+
+displayWord : Model -> Html Msg
+displayWord model =
+    if model.checked then
+        h3 [] [ text (model.current_word)]
+    else
+        h3 [] []
+
+
+displayDefinition : Definition -> Html Msg
 displayDefinition definition =
     div []
         [ h3 [] [ text definition.partOfSpeech ]
         , ul [] (List.map displaySingleDefinition definition.definitions)
         ]
 
-displaySingleDefinition : String -> Html msg
+displaySingleDefinition : String -> Html Msg
 displaySingleDefinition singleDefinition =
     li [] [ text singleDefinition ]
 
