ad1bfb506d6927593abfad2a702bbd006d392162
Fab-Den <denoyelle.fabien@gmail.com>
2024-01-26 10:40:24 +0100

    Add prompt

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,10 @@
+const readline = require('readline');
+
+const readline_interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 const draw_pile = {
     "A": 14,
     "B": 4,
@@ -31,6 +38,21 @@ const grids = [[], []]
 
 const hands= [[], []]
 
+const turn = 0
+
+const player= 1
+
+const end= false
+
+
+function prompt(message, callback) {
+    readline_interface.question(message, (a) => {
+        callback(a);
+    })
+}
+
+
+
 // Debut du tour
 // 2 x Choix de Jarnac (pas le premier)
 //  -> selectionne ligne sur la grille adverse
@@ -46,3 +68,14 @@ const hands= [[], []]
 //  -> passer (fin du tour)
 // pour chaque choix -> test si fin de partie
 
+
+// list functions
+// -> compter les points
+// -> tester la fin de partie
+// -> piocher lettres + retirer de la pioche
+// -> tester validité mot
+// -> modification d'une ligne
+// -> modification main (en paramètre les lettres retirées)
+// ->
+
+// ne pas oublier de fermer l'interface à la fin du jeu
